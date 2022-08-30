ff5da1eb7b1f6b20943b6ced95beabc0d1fbcd2b
ricardo-f <riclima1990@gmail.com>
2022-08-30 20:47:03 -0300

    last lessons from template engine, using partials and css

--- 04_templateEngine/index.js
+++ 04_templateEngine/index.js
@@ -2,10 +2,18 @@ const express = require("express")
 const exphbs = require ("express-handlebars")
 const app = express()
 
+
+
 const user = { name: "Joe", surname: "Doe" }
 
-app.engine('handlebars', exphbs.engine())
+const hbs = exphbs.create({
+  partialsDir: ["views/partials"]
+})
+
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
 
 app.get('/dashboard', (req, res) => {
   const items = ["abc", "def", "ghi"]
@@ -20,6 +28,31 @@ app.get('/blog', (req, res)=> {
     comments: 4,
   }
   res.render('blog', {post})
+})
+
+
+app.get('/post', (req,res) => {
+  const posts = [
+    {
+      title: 'This is a title 1',
+      category: 'category content 1',
+      body: 'this is the content of a blog post 1',
+      comments: 1,
+    },
+    {
+      title: 'This is a title 2',
+      category: 'category content 2',
+      body: 'this is the content of a blog post 2',
+      comments: 2,
+    },
+    {
+      title: 'This is a title 2',
+      category: 'category content 2',
+      body: 'this is the content of a blog post 2',
+      comments: 3,
+    }
+  ]
+  res.render('blogposts', {posts} )
 })
 
 app.get('/', (req, res)=> { 
