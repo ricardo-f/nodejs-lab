a4d89b1b916de6a863f71c8d55c4bbc036d74f8b
ricardo-f <riclima1990@gmail.com>
2022-09-10 15:12:33 -0300

    Init MVC arch lessons

---
+++ 07_mvc/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodemvc', 'root', '', {
+  host: 'localhost',
+  port: '13306',
+  dialect: 'mysql'
+})
+
+try {
+  sequelize.authenticate()
+  console.log('Connection with MySql was sucessful')
+} catch (error) {
+  console.log(`Unable to connect: ${error}`)
+}
+
+exports.default = sequelize
