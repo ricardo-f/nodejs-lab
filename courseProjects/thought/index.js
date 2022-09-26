aafd3ca4995f3082c25bbec36b9144eb7d2ae647
ricardo-f <riclima1990@gmail.com>
2022-09-26 11:16:14 -0300

    Importing models, routes and views, also testing basic views

--- courseProjects/thought/index.js
+++ courseProjects/thought/index.js
@@ -5,8 +5,67 @@ const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
 
 const app = express()
-
+// Models
+const Thought = require('./models/Thoughts')
+const User = require('./models/User')
+// Importing Routes
+const thoughtsRoutes = require('./routes/thoughtsRoutes')
+// Importing Controller
+const ThoughtController = require('./controllers/ThoughtController')
+// Database connection
 const conn = require('./db/conn')
+
+// static content - pub path
+app.use(express.static('public'))
+
+// Template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+// Read response from body
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+// Session middleware
+app.use(
+  session({
+    name: "sessions",
+    secret: "supaMegaSecret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () { },
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now(), + 360000),
+      // Only for lab purpose
+      httpOnly: true
+    }
+  }),
+)
+
+// Flash msgs
+app.use(flash())
+
+// Logic for session
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session
+  }
+  next()
+})
+
+// Routes
+app.use('/thoughts', thoughtsRoutes)
+app.get('/', ThoughtController.showThoughts)
 
 conn.sync()
   .then(() => {
