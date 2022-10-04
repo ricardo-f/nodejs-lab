4b324b74cf6eb9061e7beb605aaa31adba516586
ricardo-f <riclima1990@gmail.com>
2022-10-03 22:42:59 -0300

    adding new functions

--- courseProjects/thought/controllers/ThoughtController.js
+++ courseProjects/thought/controllers/ThoughtController.js
@@ -5,4 +5,42 @@ module.exports = class ThoughtController {
   static async showThoughts(req, res) {
     res.render('thoughts/home')
   }
+
+  static async dashboard(req, res) {
+    const userId = req.session.userid
+
+    const user = await User.findOne({
+      where: { 
+        id: userId,
+      },
+      include: Thought,
+      plain: true
+    })
+    if (!user) {
+      res.redirect('/login')
+    }
+    const thought = user.Thoughts.map((results) => results.dataValues)
+
+    res.render('thoughts/dashboard', {thought})
+  }
+
+  static createThoughts(req, res) {
+    res.render('thoughts/create')
+  }
+
+  static async createThoughtsSave(req, res) {
+    const thought = {
+      title: req.body.title,
+      UserId: req.session.userid
+    }
+    try {
+      await Thought.create(thought)
+      req.flash('message', 'Thought posted :)')
+      req.session.save(() => {
+        res.redirect('/thoughts/dashboard')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
