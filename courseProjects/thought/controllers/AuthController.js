1335eb3a8cad1bc27b4976dc3291c190d1d90b5d
ricardo-f <riclima1990@gmail.com>
2022-10-03 22:42:59 -0300

    creting login func

--- courseProjects/thought/controllers/AuthController.js
+++ courseProjects/thought/controllers/AuthController.js
@@ -6,14 +6,39 @@ module.exports = class AuthController {
     res.render('auth/login')
   }
 
+  static async loginPost(req, res) {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ where: { email: email } })
+    if (!user) {
+      req.flash('message', "User not found")
+      res.render('auth/login')
+      return
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.password)
+    if (!passwordMatch) {
+      req.flash('message', "Invalid Password")
+      res.render('auth/login')
+      return
+    }
+
+    req.session.userid = user.id
+    req.flash('message', 'Welcome to your personal account!')
+    req.session.save(() => {
+      res.redirect('/')
+    })
+
+  }
+
   static register(req, res) {
     res.render('auth/register')
   }
-  
+
   static async registerPost(req, res) {
     const { name, email, password, confirmpassword } = req.body
     if (password != confirmpassword) {
-      req.flash('message', "passwords don't match!")
+      req.flash('message', "Passwords don't match!")
       res.render('auth/register')
       return
     }
@@ -36,7 +61,7 @@ module.exports = class AuthController {
       const createdUser = await User.create(user)
       req.session.userid = createdUser.id
       req.flash('message', 'Register sucessfully created')
-      req.session.save(()=> {
+      req.session.save(() => {
         res.redirect('/')
       })
     } catch (error) {
@@ -44,7 +69,7 @@ module.exports = class AuthController {
     }
   }
 
-  static logout (req,res){
+  static logout(req, res) {
     req.session.destroy()
     res.redirect('/login')
   }
