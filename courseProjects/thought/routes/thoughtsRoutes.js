609b620a133918b39361d3f1ca843a91c7d7deec
ricardo-f <riclima1990@gmail.com>
2022-10-03 22:42:59 -0300

    adding routes and helper

--- courseProjects/thought/routes/thoughtsRoutes.js
+++ courseProjects/thought/routes/thoughtsRoutes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const ThoughtController = require('../controllers/ThoughtController')
 
+// helper
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add', checkAuth, ThoughtController.createThoughts)
+router.post('/add', checkAuth, ThoughtController.createThoughtsSave)
+router.get('/dashboard', checkAuth, ThoughtController.dashboard)
 router.get('/', ThoughtController.showThoughts)
 
 module.exports = router
