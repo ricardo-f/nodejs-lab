e3e1323965ad46f1a5d7b08d801b505a81cb652e
ricardo-f <riclima1990@gmail.com>
2022-10-06 20:12:28 -0300

    ending thought project

--- courseProjects/thought/controllers/ThoughtController.js
+++ courseProjects/thought/controllers/ThoughtController.js
@@ -1,16 +1,47 @@
 const Thought = require('../models/Thoughts')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ThoughtController {
   static async showThoughts(req, res) {
-    res.render('thoughts/home')
+    let search = ''
+
+    if (req.query.search) {
+      search = req.query.search
+    }
+
+    let order = 'DESC'
+
+    if (req.query.order === 'old') {
+      order = 'ASC'
+    } else {
+      order = 'DESC'
+    }
+
+    const thoughtData = await Thought.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [['createdAt', order]],
+    })
+
+    const thought = thoughtData.map((results) => results.get({ plain: true }))
+    let thoughtQty = thought.length
+
+    if (thoughtQty === 0) {
+      thoughtQty = false;
+    }
+
+    res.render('thoughts/home', { thought, search, thoughtQty })
   }
 
   static async dashboard(req, res) {
     const userId = req.session.userid
 
     const user = await User.findOne({
-      where: { 
+      where: {
         id: userId,
       },
       include: Thought,
@@ -21,7 +52,13 @@ module.exports = class ThoughtController {
     }
     const thought = user.Thoughts.map((results) => results.dataValues)
 
-    res.render('thoughts/dashboard', {thought})
+    let emptyThoughts = false
+
+    if (thought.length === 0) {
+      emptyThoughts = true
+    }
+
+    res.render('thoughts/dashboard', { thought, emptyThoughts })
   }
 
   static createThoughts(req, res) {
@@ -36,6 +73,42 @@ module.exports = class ThoughtController {
     try {
       await Thought.create(thought)
       req.flash('message', 'Thought posted :)')
+      req.session.save(() => {
+        res.redirect('/thoughts/dashboard')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  static async removeThoughts(req, res) {
+    const id = req.body.id
+    const userId = req.session.userid
+    try {
+      await Thought.destroy({ where: { id: id, UserId: userId } })
+      req.flash('message', 'Thought removed')
+      req.session.save(() => {
+        res.redirect('/thoughts/dashboard')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  static async updateThoughts(req, res) {
+    const id = req.params.id
+    const thought = await Thought.findOne({ where: { id: id }, raw: true })
+    res.render('thoughts/edit', { thought })
+  }
+
+  static async updateThoughtsSave(req, res) {
+    const id = req.body.id
+    const thought = {
+      title: req.body.title
+    }
+    try {
+      await Thought.update(thought, { where: { id: id } })
+      req.flash('message', 'Thought updated')
       req.session.save(() => {
         res.redirect('/thoughts/dashboard')
       })
--- courseProjects/thought/routes/thoughtsRoutes.js
+++ courseProjects/thought/routes/thoughtsRoutes.js
@@ -7,7 +7,10 @@ const checkAuth = require('../helpers/auth').checkAuth
 
 router.get('/add', checkAuth, ThoughtController.createThoughts)
 router.post('/add', checkAuth, ThoughtController.createThoughtsSave)
+router.get('/edit/:id', checkAuth, ThoughtController.updateThoughts)
+router.post('/edit', checkAuth, ThoughtController.updateThoughtsSave)
 router.get('/dashboard', checkAuth, ThoughtController.dashboard)
+router.post('/remove', checkAuth, ThoughtController.removeThoughts)
 router.get('/', ThoughtController.showThoughts)
 
 module.exports = router
